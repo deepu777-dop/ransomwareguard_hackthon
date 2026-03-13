7bac7942359c27aa7c0077a83790cec685acc1d7
Deepak <srinivasdeeppu143@gmail.com>
2026-03-13 14:48:18 +0200

    First commit

---
+++ RansomGuard_Final/backend/modules/__init__.py
@@ -0,0 +1,5 @@
+from .file_monitor import FileMonitor
+from .process_monitor import ProcessMonitor
+from .anomaly_detector import AnomalyDetector
+from .entropy_analyzer import EntropyAnalyzer
+from .risk_scorer import RiskScorer, AlertManager
